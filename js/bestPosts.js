99e86b7f8641ac6cf0e7d4d86e88b4e4fedf43a5
Sasha Ichkitidze <frobe11@mail.ru>
2024-12-07 03:40:27 +0300

    done lab 6 + lab 7

--- js/bestPosts.js
+++ js/bestPosts.js
@@ -1,39 +1,58 @@
-document.getElementById("fetch-images").addEventListener("click", async () => {
-  const images = await response.json();
-  images.appendChild(
-    `<img style="min-width=60vw; min-height=60vh;" src="https://tenor.com/bqj6A.gif">`
-  );
+import { requestImages } from "./requestImages.js";
+import { createImageElement } from "./createImageElement.js";
 
-  const channelTag = "sanyaich";
-  const response = await fetch(
-    `http://localhost:1489/fetch-images/${channelTag}`,
-    {
-      method: "GET",
-      mode: "cors",
-    }
-  );
+const channelTag = "sanyaich";
+const step = 10;
+const sort = 1;
+const fetchButton = document.getElementById("fetch-images")
+let count = 0;
 
-  const imagesContainer = document.getElementById("images");
-  imagesContainer.innerHTML = "";
-  let count = 0;
-  images
-    .sort((first, second) => second.rating - first.rating)
-    .forEach((image) => {
-      const div = document.createElement("div");
-      div.className = `content__section content__section_text-centered content__section_padding-l ${
-        count % 2 == 0
-          ? "bg-color_coders-gray color_coders-green"
-          : "color_coders-gray bg-color_coders-green"
-      }`;
-      div.innerHTML = `
-            <img style="width=60%; height=60%; flex:0 0 auto;" src="\\tg-photo-fetcher\\${
-              image.filePath
-            }">
-            <p>
-            <h>${image.caption || "Без подписи"}</h>
-            <p>Рейтинг: ${image.rating}</p>
-        `;
-      imagesContainer.appendChild(div);
-      count++;
-    });
+
+document.addEventListener("DOMContentLoaded", async () => {
+  fetchButton.classList.add("button_hidden")
+  const imagesContainer = document.getElementById("imagesId");
+  const preloaderContainer = document.getElementById("preloader");
+  localStorage.setItem("end", 0);
+
+  const preloader = document.createElement("img");
+  preloader.src = "images/preloader.gif";
+  preloaderContainer.appendChild(preloader);
+
+
+  const images = await requestImages(channelTag, 0, step, sort);
+
+  preloaderContainer.removeChild(preloaderContainer.firstChild);
+
+  images.forEach((image) => {
+    const div = createImageElement(image, count);
+    imagesContainer.appendChild(div);
+    count++
+  });
+  fetchButton.classList.remove("button_hidden")
+  localStorage.setItem("end", step);
+});
+
+fetchButton.addEventListener("click", async () => {
+  fetchButton.classList.add("button_hidden")
+  const start = Number(localStorage.getItem("end"));
+  const end = start + step;
+  const imagesContainer = document.getElementById("imagesId");
+  const preloaderContainer = document.getElementById("preloader");
+
+  const preloader = document.createElement("img");
+  preloader.src = "images/preloader.gif";
+  preloaderContainer.appendChild(preloader);
+
+
+  const images = await requestImages(channelTag, start, end, sort);
+
+  preloaderContainer.removeChild(preloaderContainer.firstChild);
+
+  images.forEach((image) => {
+    const div = createImageElement(image, count);
+    imagesContainer.appendChild(div);
+    count++
+  });
+  fetchButton.classList.remove("button_hidden")
+  localStorage.setItem("end", end);
 });
--- js/worstPosts.js
+++ js/worstPosts.js
@@ -1,24 +1,58 @@
-document.getElementById('fetch-images').addEventListener('click', async () => {
-    const channelTag = 'sanyaich';
-    const response = await fetch(`http://localhost:1489/fetch-images/${channelTag}`, {
-        method: 'GET',
-        mode: 'cors'
-    });
-    const images = await response.json();
+import { requestImages } from "./requestImages.js";
+import { createImageElement } from "./createImageElement.js";
 
-    const imagesContainer = document.getElementById('images');
-    imagesContainer.innerHTML = '';
-    let count = 0;
-    images.sort((first, second) => first.rating - second.rating).forEach(image => {
-        const div = document.createElement('div');
-        div.className = `content__section content__section_text-centered content__section_padding-l ${count % 2 == 0 ? 'bg-color_coders-gray color_coders-green': 'color_coders-gray bg-color_coders-green'}`;
-        div.innerHTML = `
-            <img width="60%" height="40%" src="\\tg-photo-fetcher\\${image.filePath}">
-            <p>
-            <h>${image.caption || 'Без подписи'}</h>
-            <p>Рейтинг: ${image.rating}</p>
-        `;
-        imagesContainer.appendChild(div);
-        count++;
+const channelTag = "sanyaich";
+const step = 10;
+const sort = -1;
+const fetchButton = document.getElementById("fetch-images")
+let count = 0;
+
+
+document.addEventListener("DOMContentLoaded", async () => {
+    fetchButton.classList.add("button_hidden")
+    const imagesContainer = document.getElementById("imagesId");
+    const preloaderContainer = document.getElementById("preloader");
+    localStorage.setItem("end", 0);
+  
+    const preloader = document.createElement("img");
+    preloader.src = "images/preloader.gif";
+    preloaderContainer.appendChild(preloader);
+  
+  
+    const images = await requestImages(channelTag, 0, step, sort);
+  
+    preloaderContainer.removeChild(preloaderContainer.firstChild);
+  
+    images.forEach((image) => {
+      const div = createImageElement(image, count);
+      imagesContainer.appendChild(div);
+      count++
     });
-});
+    fetchButton.classList.remove("button_hidden")
+    localStorage.setItem("end", step);
+  });
+  
+  fetchButton.addEventListener("click", async () => {
+    fetchButton.classList.add("button_hidden")
+    const start = Number(localStorage.getItem("end"));
+    const end = start + step;
+    const imagesContainer = document.getElementById("imagesId");
+    const preloaderContainer = document.getElementById("preloader");
+  
+    const preloader = document.createElement("img");
+    preloader.src = "images/preloader.gif";
+    preloaderContainer.appendChild(preloader);
+  
+  
+    const images = await requestImages(channelTag, start, end, sort);
+  
+    preloaderContainer.removeChild(preloaderContainer.firstChild);
+  
+    images.forEach((image) => {
+      const div = createImageElement(image, count);
+      imagesContainer.appendChild(div);
+      count++
+    });
+    fetchButton.classList.remove("button_hidden")
+    localStorage.setItem("end", end);
+  });
