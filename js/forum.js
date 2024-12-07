15582f1fdbb6f80d37066bb59305afd67868edc1
Sasha Ichkitidze <frobe11@mail.ru>
2024-12-07 15:57:06 +0300

    афые

--- js/forum.js
+++ js/forum.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const nickHolder = document.getElementById("nickname");
   nickname = localStorage.getItem("nickname");
   if (nickname) {
-    nickHolder.value = nickname
+    nickHolder.value = nickname;
   }
 });
 
@@ -10,46 +10,61 @@ document
   .getElementById("og-forum-form")
   .addEventListener("submit", function (event) {
     event.preventDefault();
+    const container = document.getElementById("og-messages-container");
+
     const theme = document.getElementById("theme").value
       ? document.getElementById("theme").value
       : "Без темы";
+
     const messageText = document.getElementById("message-text").value;
+
     const messageImage = document.getElementById("message-image").files[0];
     const messageImageUrl = messageImage
       ? URL.createObjectURL(messageImage)
       : undefined;
+
     const nickname = document.getElementById("nickname").value
       ? document.getElementById("nickname").value
       : "anon";
+
     const avatar = document.getElementById("avatar").files[0];
     const avatarUrl = avatar
       ? URL.createObjectURL(avatar)
       : "images/anon_ava.png";
-    const container = document.getElementById("og-messages-container");
-    
-    localStorage.removeItem("nickname")
+
+    localStorage.removeItem("nickname");
     localStorage.setItem("nickname", nickname);
-    
+
     const newMessage = document.createElement("div");
     newMessage.classList.add("message");
     newMessage.classList.add("bg-color_coders-gray");
     newMessage.classList.add("color_coders-green");
-    const photo = `<img class="message__image" src="${messageImageUrl}">`;
-    console.log(messageImageUrl ? photo : "");
-    newMessage.innerHTML = `
-        <h2 class="message__theme">${theme}</h2>
-        <p class="message__text">${messageText}</p>
 
-        ${messageImageUrl ? photo : ""}
+    const themeEl = document.createElement("h2");
+    themeEl.classList.add("message__theme");
+    themeEl.textContent = theme;
 
-        <div class="message__footer">
-            <p class="message__nickname">${nickname}</p>
-            <img class="message__avatar" src="${avatarUrl}" style="max-width: 108px; height: 108px">
-        </div>
-        
-        
-    `;
+    const messageTextEl = document.createElement("p");
+    messageTextEl.classList.add("message__text");
+    messageTextEl.textContent = messageText;
+
+    const messageImageEl = document.createElement("img");
+    messageImageEl.classList.add("message__image");
+    messageImageEl.src = messageImageUrl;
+
+    const nicknameEl = document.createElement("p");
+    nicknameEl.classList.add("message__nickname");
+    nicknameEl.textContent = nickname;
+
+    const avatarEl = document.createElement("img");
+    avatarEl.classList.add("message__avatar");
+    avatarEl.src = avatarUrl;
+
+    newMessage.appendChild(themeEl);
+    newMessage.appendChild(messageTextEl);
+    newMessage.appendChild(messageImageEl);
+    newMessage.appendChild(nicknameEl);
+    newMessage.appendChild(avatarEl);
 
     container.appendChild(newMessage);
-
   });
